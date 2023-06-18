99733c47d806718ebc58d1338d8b30d18663f3bd
Mathis <mathis.levieux1@gmail.com>
2023-06-19 01:10:19 +0200

    update

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -150,6 +150,29 @@ export default function Meme() {
 
 // useEffect ET fonction de CLEANUP 
 
+    /**
+    useEffect takes a function as its parameter. If that function
+    returns something, it needs to be a cleanup function. Otherwise,
+    it should return nothing. If we make it an async function, it
+    automatically retuns a promise instead of a function or nothing.
+    Therefore, if you want to use async operations inside of useEffect,
+    you need to define the function separately inside of the callback
+    function, as seen below:
+    */
+
+// AVEC ASYNC ET AWAIT ET SANS CLEANUP
+
+    // React.useEffect(() => {
+    //     async function getMemes() {
+    //         const res = await fetch("https://api.imgflip.com/get_memes")
+    //         const data = await res.json()
+    //         setAllMemes(data.data.memes)
+    //     }
+    //     getMemes()
+    // }, [])
+
+// AVEC .THEN ET AVEC CLEANUP
+
 // React.useEffect(() => {
 //     function watchWidth() { 
 //         console.log("Setting up...")
